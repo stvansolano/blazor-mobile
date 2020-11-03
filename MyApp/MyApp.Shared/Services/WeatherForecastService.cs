cf9b6f86d79967417b427392b144abf14bc9ea94
Esteban Solano Granados <stvansolano@outlook.com>
2020-11-03 04:08:43 +0000

    Added Wasm WeatherForecastService

---
+++ MyApp/MyApp.Shared/Services/WeatherForecastService.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MyApp.Shared;
+using MyApp.Shared.Services;
+
+namespace MyApp.Shared.Services
+{
+	public class WeatherForecastService : IForecastService
+	{
+		public async Task<WeatherForecast[]> GetForecastAsync()
+		{
+			return await JsonSerializer.DeserializeAsync<WeatherForecast[]>(
+				new MemoryStream(System.Text.Encoding.UTF8.GetBytes(JSON))
+			);
+		}
+
+        #region JSON
+        private readonly string JSON = @"[
+        {
+          ""date"": ""2018-05-06"",
+          ""temperatureC"": 1,
+          ""summary"": ""Freezing"",
+          ""temperatureF"": 33
+        },
+        {
+          ""date"": ""2018-05-07"",
+          ""temperatureC"": 14,
+          ""summary"": ""Bracing"",
+          ""temperatureF"": 57
+        },
+        {
+          ""date"": ""2018-05-08"",
+          ""temperatureC"": -13,
+          ""summary"": ""Freezing"",
+          ""temperatureF"": 9
+        },
+        {
+          ""date"": ""2018-05-09"",
+          ""temperatureC"": -16,
+          ""summary"": ""Balmy"",
+          ""temperatureF"": 4
+        },
+        {
+          ""date"": ""2018-05-10"",
+          ""temperatureC"": -2,
+          ""summary"": ""Chilly"",
+          ""temperatureF"": 29
+        }
+      ]";
+
+		#endregion
+	}
+}
--- MyApp/MyApp.Wasm/Program.cs
+++ MyApp/MyApp.Wasm/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MyApp.Shared.Services;
 
 namespace MyApp.Wasm
 {
@@ -17,6 +18,7 @@ namespace MyApp.Wasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddScoped<IForecastService>(sp => new WeatherForecastService());
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
